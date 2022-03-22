344755148ee24736198d864c1274fae8fd8c759c
Pedro <46246172+peperone9@users.noreply.github.com>
2022-03-22 15:22:12 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace amor_e_odio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Criando as variaveis
+            string amor = "Amor";
+            string odio = "odio";
+            string amorguardado; //Variavel
+
+            //escrevendo na tela o conteudo de amor e te ódio
+            amorguardado = amor;
+            amor = odio;
+            odio = amorguardado;
+
+            Console.WriteLine($"Conteúdo da variavel amor: {amor}");
+            Console.WriteLine($"Conteúdo da variavel odio: {odio}");
+
+        }
+    }
+}
